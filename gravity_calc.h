2bfffa105705957f47207aff080e509c6b2aad50
rossini135 <toquehuk@gmail.com>
2025-06-21 00:02:55 -0300

    i'm tired boss. Dont let-me in the dark! i'm afreid of the dark - a espera de um milagre

--- gravity_calc.h
+++ gravity_calc.h
@@ -7,8 +7,8 @@
 using namespace std;
 
 float gravity_strenght(float massA, float massB, float distance) {
-    const float G = std::pow(6.67, (-11)); // Gravitational constant in m^3 kg^-1 s^-2
-    float gratational_force = G*((massA*massB)/std::pow(distance, 2));
+    const float G = pow(6.67, (-11)); // Gravitational constant in m^3 kg^-1 s^-2
+    float gratational_force = G*((massA*massB)/pow(distance, 2));
     return gratational_force;
 }
 
@@ -18,9 +18,28 @@ float DeltaV_calc(float eng_impulse, float gravity_aceleration, float FullMass, 
 }
 
 float reach_orbit_calc(float astro_mass, float distance_to_planet_center){
-    const float G = std::pow(6.67, (-11));
+    const float G = pow(6.67, (-11));
     float NeededVel = ((G*astro_mass)/distance_to_planet_center);
     return NeededVel;
+}
+
+float gravity_art_satelit(float Max_mass , float Min_mass, float distance){
+    const float G = pow(6.67, (-11));
+    float v = sqrt((G + ((Min_mass+Max_mass)/2))/distance);
+    return v;
+}
+
+float angular_velocity_angular_momenty(float time_of_revolution){
+    float pi = 3.14159;
+    float omega = ((2*pow(pi, 2))/pow(time_of_revolution, 2));
+    return omega;
+}
+
+float orbit_period_time(float massF, float massE, float distance){
+    const float G = pow(6.67, (-11));
+    float pi  = 3.14159;
+    float t = sqrt((4*pow(pi, 2))/G*((massE+massF)/2)*pow(distance, 3));
+    return t;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,23 +5,56 @@ using namespace std;
 int main() {
     float mass_terra = 59.72;
     float eng_imp, gravity_acell, MassF, MassE;
-    cout << "forca do motor: " << endl;
-    cin >> eng_imp; 
-    cout << "aceleracao da gravidade: " << endl;
-    cin >> gravity_acell;
-    cout << "Massa total: " << endl;
-    cin >> MassF;
-    cout << "Massa vazia: " << endl;
-    cin >> MassE;
+    int opcao;
+    printf("opções\n1) calc de deltaV \n2) satelite gravitational force\n3) angular velocity\nopção: ");
+    cin >> opcao;
+    if(opcao == 1){
+        cout << "forca do motor: " << endl;
+        cin >> eng_imp; 
+        cout << "aceleracao da gravidade: " << endl;
+        cin >> gravity_acell;
+        cout << "Massa total: " << endl;
+        cin >> MassF;
+        cout << "Massa vazia: " << endl;
+        cin >> MassE;
 
-    float deltaV = DeltaV_calc(eng_imp, gravity_acell, MassF, MassE);
-    float orbit_needed = reach_orbit_calc(mass_terra, 35);
-    cout << "DeltaV: " << deltaV << endl;
-    cout << "nescessario para orbita: " << orbit_needed << endl;
-    if(orbit_needed > deltaV){
-        cout << "não é possivel ir a orbita!" << endl;
-    }else{
-        cout << "é possivel ir a orbita!" << endl;
+        float deltaV = DeltaV_calc(eng_imp, gravity_acell, MassF, MassE);
+        float orbit_needed = reach_orbit_calc(mass_terra, 35);
+
+        cout << "DeltaV: " << deltaV << endl;
+        cout << "nescessario para orbita: " << orbit_needed << endl;
+
+        if(orbit_needed > deltaV){
+            cout << "não é possivel ir a orbita!" << endl;
+        }else{
+            cout << "é possivel ir a orbita!" << endl;
+        }
+    }else if(opcao == 2){
+        float massF, massE, distance, gravity_str;
+        cout << "Massa total: " << endl;
+        cin >> massF;
+        cout << "Massa limpa: " << endl;
+        cin >> massE;
+        cout << "distancia: " << endl;
+        cin >> distance;
+        gravity_str = gravity_strenght(massF, massE, distance);
+        float res = gravity_art_satelit(massF, massE, distance);
+        cout << "força gravitacional do satelite: " << res << endl;
+    }else if(opcao == 3){
+        float massF, massE, distance;
+        cout << "Massa total: " << endl;
+        cin >> MassF;
+        cout << "Massa vazia: " << endl;
+        cin >> MassE;
+        cout << "distancia: " << endl;
+        cin >> distance;
+
+        float time = orbit_period_time(massF, massE, distance);
+        float velocity = angular_velocity_angular_momenty(time);
+        cout << "velocity on corpse: " << velocity << " (m/s)" <<endl;
+        cout << "time to revolution: " << time << (" (Hours)")<<endl; 
     }
+
+
     return 0;
 }
